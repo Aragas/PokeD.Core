572e866b4b3d5a647b8c163b279c58e3c69f892f
Aragas <micvitalij@gmail.com>
2015-09-19 23:09:10 +0300

    Added SCON packets

---
+++ Packets/SCON/Chat/ChatMessagePacket.cs
@@ -0,0 +1,19 @@
+﻿using PokeD.Core.Interfaces;
+
+namespace PokeD.Core.Packets.SCON.Status
+{
+    public class StopChatReceivingPacket : ProtobufPacket
+    {
+        public override int ID => (int)SCONPacketTypes.PlayerListRequest;
+
+        public override ProtobufPacket ReadPacket(IPacketDataReader reader)
+        {
+            return this;
+        }
+
+        public override ProtobufPacket WritePacket(IPacketStream stream)
+        {
+            return this;
+        }
+    }
+}
---
+++ Packets/SCON/Status/PlayerLocationResponsePacket.cs
@@ -0,0 +1,21 @@
+﻿using PokeD.Core.Interfaces;
+
+namespace PokeD.Core.Packets.SCON.Status
+{
+    public class PlayerLocationRequestPacket : ProtobufPacket
+    {
+        public string Player { get; set; }
+
+        public override int ID => (int) SCONPacketTypes.PlayerListRequest;
+
+        public override ProtobufPacket ReadPacket(IPacketDataReader reader)
+        {
+            return this;
+        }
+
+        public override ProtobufPacket WritePacket(IPacketStream stream)
+        {
+            return this;
+        }
+    }
+}
