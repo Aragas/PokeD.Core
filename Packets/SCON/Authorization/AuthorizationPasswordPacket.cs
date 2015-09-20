2fd22d12656d464b5ea4ca3a19ef4723a66c2716
Aragas <micvitalij@gmail.com>
2015-09-20 23:12:59 +0300

    pass fix

--- Packets/SCON/Authorization/AuthorizationPasswordPacket.cs
+++ Packets/SCON/Authorization/AuthorizationPasswordPacket.cs
@@ -10,11 +10,15 @@ namespace PokeD.Core.Packets.SCON.Authorization
 
         public override ProtobufPacket ReadPacket(IPacketDataReader reader)
         {
+            Password = reader.ReadString();
+
             return this;
         }
 
         public override ProtobufPacket WritePacket(IPacketStream stream)
         {
+            stream.WriteString(Password);
+
             return this;
         }
     }
