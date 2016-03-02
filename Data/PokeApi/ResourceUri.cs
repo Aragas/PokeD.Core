b5f833d20fd3b1fd418feadaa610fa10f6b65974
Aragas <micvitalij@yandex.ru>
2016-03-03 02:43:37 +0300

    Update

--- Data/PokeApi/ResourceUri.cs
+++ Data/PokeApi/ResourceUri.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Linq;
-
+using Aragas.Core.Extensions;
 using PokeD.Core.Extensions;
 
 namespace PokeD.Core.Data.PokeApi
--- Data/PokeD/Structs/MetaPosition.cs
+++ Data/PokeD/Structs/MetaPosition.cs
@@ -9,7 +9,7 @@ namespace PokeD.Core.Data.PokeD.Structs
         
         public Vector3 Position
         {
-            get { return Vector3.FromFixedPoint(Meta.BitsGet(0, 12), Meta.BitsGet(24, 32), Meta.BitsGet(12, 24)); }
+            get { return Vector3Extensions.FromFixedPoint(Meta.BitsGet(0, 12), Meta.BitsGet(24, 32), Meta.BitsGet(12, 24)); }
             set
             {
                 Meta = Meta.BitsSet((long) (value.X * 32.0f), 0, 12);
