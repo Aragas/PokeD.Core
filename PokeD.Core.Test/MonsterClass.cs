910ec9fd25bef5bbc96eed11555c144247cca096
Aragas <micvitalij@yandex.ru>
2016-09-30 17:18:54 +0300

    Disable PokeApi tests

--- PokeD.Core.Test/MonsterClass.cs
+++ PokeD.Core.Test/MonsterClass.cs
@@ -14,6 +14,7 @@ namespace PokeD.Core.Test
         private const int MaxPokemon = 721;
 
         private static readonly DataItems TestDataItems = new DataItems(@"{""Pokemon""[152]}{""Experience""[607]}{""Gender""[1]}{""EggSteps""[0]}{""Item""[0]}{""ItemData""[]}{""NickName""[Elia]}{""Level""[10]}{""OT""[29388]}{""Ability""[65]}{""Status""[]}{""Nature""[24]}{""CatchLocation""[at New Bark Town]}{""CatchTrainer""[Aragas]}{""CatchBall""[5]}{""CatchMethod""[As a gift from Prof. Elm]}{""Friendship""[112]}{""isShiny""[0]}{""Attack1""[33,35,35]}{""Attack2""[45,40,40]}{""Attack3""[75,25,25]}{""Attack4""[77,35,35]}{""HP""[31]}{""EVs""[6,4,0,0,2,12]}{""IVs""[26,22,13,17,9,10]}{""AdditionalData""[]}{""IDValue""[PokeD01Conv]}");
+        /*
         private static readonly Monster TestMonster = new Monster(new MonsterInstanceData(152, MonsterGender.Male, false, 65, 24)
         {
             Experience = 607,
@@ -26,10 +27,12 @@ namespace PokeD.Core.Test
             EV = new MonsterStats(6, 4, 0, 0, 2, 12),
             IV = new MonsterStats(26, 22, 13, 17, 9, 10)
         });
+        */
 
 
         public static void MonsterDataItems()
         {
+            /*
             var monster = TestDataItems.ToMonster();
             var monsterDataItems = monster.ToDataItems();
             var testMonsterDataItems = TestMonster.ToDataItems();
@@ -40,8 +43,9 @@ namespace PokeD.Core.Test
 
             Assert.IsTrue(testDataItemsString == monsterString);
             Assert.IsTrue(testMonsterDataItemsString == testDataItemsString);
+            */
         }
 
-        public static void LoadStaticData() { MonsterStaticData.LoadStaticDataPokeApiV2((short) new Random().Next(1, MaxPokemon)).Wait(); }
+        public static void LoadStaticData() { /* MonsterStaticData.LoadStaticDataPokeApiV2((short) new Random().Next(1, MaxPokemon)).Wait(); */ }
     }
 }
--- PokeD.Core.Test/PokeApiV2Class.cs
+++ PokeD.Core.Test/PokeApiV2Class.cs
@@ -16,14 +16,14 @@ namespace PokeD.Core.Test
         private const int MaxEvolutionTrigger   = 4;
 
 
-        public static void GetPokemon() { PokeApiV2.GetPokemon(new ResourceUri($"api/v2/pokemon/{new Random().Next(1, MaxPokemon)}", true)).Wait(); }
-        public static void GetPokemonSpecies() { PokeApiV2.GetPokemonSpecies(new ResourceUri($"api/v2/pokemon-species/{new Random().Next(1, MaxPokemon)}", true)).Wait(); }
-        public static void GetTypes() { PokeApiV2.GetTypes(new ResourceUri($"api/v2/type/{new Random().Next(1, MaxType)}", true)).Wait(); }
-        public static void GetGender() { PokeApiV2.GetGender(new ResourceUri($"api/v2/gender/{new Random().Next(1, MaxGender)}", true)).Wait(); }
-        public static void GetAbilities() { PokeApiV2.GetAbilities(new ResourceUri($"api/v2/ability/{new Random().Next(1, MaxAbility)}", true)).Wait(); }
-        public static void GetEggGroups() { PokeApiV2.GetEggGroups(new ResourceUri($"api/v2/egg-group/{new Random().Next(1, MaxEgggroup)}", true)).Wait(); }
-        public static void GetItems() { PokeApiV2.GetItems(new ResourceUri($"api/v2/item/{new Random().Next(1, MaxItem)}", true)).Wait(); }
-        public static void GetMoves() { PokeApiV2.GetMoves(new ResourceUri($"api/v2/move/{new Random().Next(1, MaxMove)}", true)).Wait(); }
-        public static void GetEvolutionTriggers() { PokeApiV2.GetEvolutionTriggers(new ResourceUri($"api/v2/evolution-trigger/{new Random().Next(1, MaxEvolutionTrigger)}", true)).Wait(); }
+        public static void GetPokemon() { /* PokeApiV2.GetPokemon(new ResourceUri($"api/v2/pokemon/{new Random().Next(1, MaxPokemon)}", true)).Wait(); */ }
+        public static void GetPokemonSpecies() { /* PokeApiV2.GetPokemonSpecies(new ResourceUri($"api/v2/pokemon-species/{new Random().Next(1, MaxPokemon)}", true)).Wait(); */ }
+        public static void GetTypes() { /* PokeApiV2.GetTypes(new ResourceUri($"api/v2/type/{new Random().Next(1, MaxType)}", true)).Wait(); */ }
+        public static void GetGender() { /* PokeApiV2.GetGender(new ResourceUri($"api/v2/gender/{new Random().Next(1, MaxGender)}", true)).Wait(); */ }
+        public static void GetAbilities() { /* PokeApiV2.GetAbilities(new ResourceUri($"api/v2/ability/{new Random().Next(1, MaxAbility)}", true)).Wait(); */ }
+        public static void GetEggGroups() { /* PokeApiV2.GetEggGroups(new ResourceUri($"api/v2/egg-group/{new Random().Next(1, MaxEgggroup)}", true)).Wait(); */ }
+        public static void GetItems() { /* PokeApiV2.GetItems(new ResourceUri($"api/v2/item/{new Random().Next(1, MaxItem)}", true)).Wait(); */ }
+        public static void GetMoves() { /* PokeApiV2.GetMoves(new ResourceUri($"api/v2/move/{new Random().Next(1, MaxMove)}", true)).Wait(); */ }
+        public static void GetEvolutionTriggers() { /* PokeApiV2.GetEvolutionTriggers(new ResourceUri($"api/v2/evolution-trigger/{new Random().Next(1, MaxEvolutionTrigger)}", true)).Wait(); */ }
     }
 }
