a6a77bbf7663b7cc97d33675e06bd1e18279119a
Aragas <micvitalij@yandex.ru>
2017-07-02 04:38:33 +0300

    Handling a bit different socket clients, part of the PokeD.Server experiment

--- PokeD.Core/IO/P3DTransmission.cs
+++ PokeD.Core/IO/P3DTransmission.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 
 using Aragas.Network.IO;
@@ -11,9 +14,40 @@ using PokeD.Core.Packets.P3D;
 
 namespace PokeD.Core.IO
 {
+    public class BetterSocketStream : NetworkStream
+    {
+        public BetterSocketStream(Socket socket) : base(socket) { }
+
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            try { base.Write(buffer, offset, count); }
+            catch (IOException) { return; }
+            catch (SocketException) { return; }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            try { return base.Read(buffer, offset, count); }
+            catch (IOException) { return -1; }
+            catch (SocketException) { return -1; }
+        }
+
+        public override int ReadByte()
+        {
+            var buffer = new byte[1];
+            return Read(buffer, 0, 1) != -1 ? buffer[0] : -1;
+        }
+    }
+
+
     public class P3DTransmission : SocketPacketTransmission<P3DPacket, int, P3DSerializer, P3DDeserializer>
     {
-        public P3DTransmission(ISocketClient socket, Type packetEnumType = null) : base(socket, packetEnumType) { }
+        public P3DTransmission(ISocketClient socketClient, Type packetEnumType = null) : base(socketClient, packetEnumType)
+        {
+            var socket = socketClient.GetType().GetProperty("Socket", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(socketClient) as Socket;
+            typeof(SocketPacketTransmission<P3DPacket, int, P3DSerializer, P3DDeserializer>).GetField("<SocketStream>k__BackingField", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(this, new BetterSocketStream(socket));
+        }
 
 
         public override void SendPacket(P3DPacket packet)
@@ -26,7 +60,7 @@ namespace PokeD.Core.IO
             if (Socket.DataAvailable > 0)
             {
                 var data = ReadLine();
-                if (P3DPacket.TryParseID(data, out int id))
+                if (P3DPacket.TryParseID(data, out var id))
                 {
                     var packet = Factory.Create(id);
                     if (packet?.TryParseData(data) == true)
@@ -37,12 +71,24 @@ namespace PokeD.Core.IO
             return null;
         }
 
+        public P3DPacket ReadPacketBlocking()
+        {
+            var data = ReadLine();
+            if (P3DPacket.TryParseID(data, out var id))
+            {
+                var packet = Factory.Create(id);
+                if (packet?.TryParseData(data) == true)
+                    return packet;
+            }
+            return null;
+        }
+
 
         private StringBuilder StringBuilder { get; } = new StringBuilder();
         private IEnumerable<string> ReadLineEnumerable()
         {
             var symbol = (char) SocketStream.ReadByte();
-            while (symbol != -1)
+            while (symbol != char.MaxValue) // -1 will be char.MaxValue
             {
                 char nextSymbol;
                 if (symbol == '\r' && Socket.DataAvailable == 0)
@@ -59,7 +105,7 @@ namespace PokeD.Core.IO
 
                     yield return line;
                 }
-                else if (nextSymbol == -1)
+                else if (nextSymbol == char.MaxValue)
                     yield return string.Empty;
                 else
                 {
@@ -67,8 +113,7 @@ namespace PokeD.Core.IO
                     symbol = nextSymbol;
                 }
 
-
-
+                
                 /*
                 var nextSymbol = (char) ReadByte();
                 if(nextSymbol == -1)
