79886e3693d1654846dcbd29594fab9005851b5b
Aragas <micvitalij@yandex.ru>
2017-07-05 17:27:44 +0300

    Second try with the keepalive thing

--- PokeD.Core/IO/P3DTransmission.cs
+++ PokeD.Core/IO/P3DTransmission.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Text;
 
 using Aragas.Network.IO;
@@ -13,19 +12,48 @@ namespace PokeD.Core.IO
 {
     public class P3DTransmission : SocketPacketTransmission<P3DPacket, int, P3DSerializer, P3DDeserializer>
     {
+        public static bool SetKeepAlive(Socket socket, ulong time, ulong interval)
+        {
+            const int BytesPerLong = 4;
+            const int BitsPerByte = 8;
+
+            var turnOn = time != 0 && interval != 0;
+            // Array to hold input values.
+            var input = new[]
+            {
+                turnOn ? 1UL : 0UL, // on or off
+                time,
+                interval
+            };
+
+            // Pack input into byte struct.
+            var inValue = new byte[3 * BytesPerLong];
+            for (var i = 0; i < input.Length; i++)
+            {
+                inValue[i * BytesPerLong + 3] = (byte) (input[i] >> ((BytesPerLong - 1) * BitsPerByte) & 0xff);
+                inValue[i * BytesPerLong + 2] = (byte) (input[i] >> ((BytesPerLong - 2) * BitsPerByte) & 0xff);
+                inValue[i * BytesPerLong + 1] = (byte) (input[i] >> ((BytesPerLong - 3) * BitsPerByte) & 0xff);
+                inValue[i * BytesPerLong + 0] = (byte) (input[i] >> ((BytesPerLong - 4) * BitsPerByte) & 0xff);
+            }
+
+            var outValue = BitConverter.GetBytes(0);
+
+            try
+            {
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, turnOn);
+                socket.IOControl(IOControlCode.KeepAliveValues, inValue, outValue);
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
         public P3DTransmission(Socket socket, Type packetEnumType = null) : base(socket, new P3DSocketStream(socket), packetEnumType)
         {
-            socket.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive);
-
-            uint dummy = 0;
-            byte[] inOptionValues = new byte[Marshal.SizeOf(dummy) * 3];
-            //set keepalive on
-            BitConverter.GetBytes((uint) 1).CopyTo(inOptionValues, 0);
-            //interval time between last operation on socket and first checking. example:5000ms=5s
-            BitConverter.GetBytes((uint) 5000).CopyTo(inOptionValues, Marshal.SizeOf(dummy));
-            //after first checking, socket will check serval times by 1000ms.
-            BitConverter.GetBytes((uint) 1000).CopyTo(inOptionValues, Marshal.SizeOf(dummy) * 2);
-            socket.IOControl(IOControlCode.KeepAliveValues, inOptionValues, null);
+            SetKeepAlive(socket, 5000, 1000);
         }
 
 
