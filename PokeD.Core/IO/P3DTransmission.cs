c10fbd16dd97e8b643e717679a930441d319d2ed
Aragas <micvitalij@yandex.ru>
2017-07-05 16:44:41 +0300

    KeepAlive test
    See https://stackoverflow.com/questions/4659215/how-to-get-status-of-socket-when-network-cable-disconnect-from-network-adapter

--- PokeD.Core/IO/P3DTransmission.cs
+++ PokeD.Core/IO/P3DTransmission.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 
 using Aragas.Network.IO;
@@ -12,7 +13,20 @@ namespace PokeD.Core.IO
 {
     public class P3DTransmission : SocketPacketTransmission<P3DPacket, int, P3DSerializer, P3DDeserializer>
     {
-        public P3DTransmission(Socket socket, Type packetEnumType = null) : base(socket, new P3DSocketStream(socket), packetEnumType) { }
+        public P3DTransmission(Socket socket, Type packetEnumType = null) : base(socket, new P3DSocketStream(socket), packetEnumType)
+        {
+            socket.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive);
+
+            uint dummy = 0;
+            byte[] inOptionValues = new byte[Marshal.SizeOf(dummy) * 3];
+            //set keepalive on
+            BitConverter.GetBytes((uint) 1).CopyTo(inOptionValues, 0);
+            //interval time between last operation on socket and first checking. example:5000ms=5s
+            BitConverter.GetBytes((uint) 5000).CopyTo(inOptionValues, Marshal.SizeOf(dummy));
+            //after first checking, socket will check serval times by 1000ms.
+            BitConverter.GetBytes((uint) 1000).CopyTo(inOptionValues, Marshal.SizeOf(dummy) * 2);
+            socket.IOControl(IOControlCode.KeepAliveValues, inOptionValues, null);
+        }
 
 
         public override void SendPacket(P3DPacket packet)
